94888d7a2b9ed64bfe7dc2efadcfd494f431ad8e
Adri <46067039+Gallantmon@users.noreply.github.com>
2022-08-09 16:51:43 +0200

    Modificación 1
    Corrección del error cuando hay dos números seguidos iguales.

--- Reto 01 - 05-08-2022/Resolucion/RetoDev1.cpp
+++ Reto 01 - 05-08-2022/Resolucion/RetoDev1.cpp
@@ -52,12 +52,12 @@ int main(){
     delete_data();
     check_data();
 
-    print_data();
+    //print_data();
     if (success) {
-        cout << "Salida: True" << endl;
+        cout << "Salida: Verdadero" << endl;
     }
     else {
-        cout << "Salida: False" << endl;
+        cout << "Salida: Falso" << endl;
     }
 }
 
@@ -95,8 +95,11 @@ void delete_data() {
 void check_data() {
     int checks = 0;
     for (int i = 1; i < contador + 1; i++) {
-        if (matrix[i - 1] <= matrix[i]) {
+        if (matrix[i - 1] < matrix[i]) {
             checks++;
+        }
+        if (matrix[i - 1] == matrix[i]) {
+            break;
         }
     }
     if (checks == contador - 1) success = true;
@@ -122,19 +125,3 @@ void print_data() {
     cout << "]" << endl;
 }
 
-
-
-
-
-
-
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
